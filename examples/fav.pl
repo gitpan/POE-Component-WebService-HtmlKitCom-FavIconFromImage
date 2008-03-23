13f094c5c454e67f82b6025d9a97edc25c21b03b
Zoffix Znet <zoffixcpan@gmail.com>
2008-03-23 16:36:22 +0000

    Import of ZOFFIX/POE-Component-WebService-HtmlKitCom-FavIconFromImage-0.001 from CPAN.
    gitpan-cpan-distribution: POE-Component-WebService-HtmlKitCom-FavIconFromImage
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ZOFFIX/POE-Component-WebService-HtmlKitCom-FavIconFromImage-0.001.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

---
+++ examples/fav.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib '../lib';
+use POE qw(Component::WebService::HtmlKitCom::FavIconFromImage);
+
+die "Usage: perl fav.pl <file_name_of_picture_to_make_favicon_from>\n"
+    unless @ARGV;
+
+my $Image = shift;
+
+my $poco = POE::Component::WebService::HtmlKitCom::FavIconFromImage->spawn;
+
+POE::Session->create(
+    package_states => [ main => [qw(_start result)] ],
+);
+
+$poe_kernel->run;
+
+sub _start {
+    $poco->favicon( {
+            image => $Image,
+            file  => 'out.zip',
+            event => 'result',
+        }
+    );
+}
+
+sub result {
+    my $in_ref = $_[ARG0];
+
+    if ( exists $in_ref->{error} ) {
+        print "Got error: $in_ref->{error}\n";
+    }
+    else {
+        print "Done! I saved your favicon in out.zip file\n";
+    }
+
+    $poco->shutdown;
+}
